dad715ed9bbe2c3f39bb72c0899b997a1b5c3e64
marisa.mahilum <marisa.mahilum@keybridgeglobal.com>
2024-01-31 09:58:46 +0800

    retrun response only

--- api/aws_llamamodel.py
+++ api/aws_llamamodel.py
@@ -11,4 +11,23 @@ def sendMessagetoLlamaModel(msg):
     response = requests.post("https://dammmnlbi0.execute-api.ap-southeast-2.amazonaws.com/default/mathison-llama-request", json=data)
     myresponse = response.json()
     print("Response", myresponse)
-    return myresponse
+
+    if isinstance(myresponse, list):
+        # Response is a list of dictionaries
+        if myresponse:
+            response_string = myresponse[0]['generated_text'].split("### Response")[-1].strip().strip('\"')
+        else:
+            print("No response found in the list.")
+    elif isinstance(myresponse, dict):
+        # Response is a single dictionary
+        if 'outputs' in myresponse and 'text' in myresponse['outputs']:
+            if myresponse['outputs']['text']:
+                response_string = myresponse['outputs']['text'][0].split("### Response")[-1].strip().strip('\"')
+            else:
+                print("No text found in the response.")
+        else:
+            print("Invalid response format.")
+    else:
+        print("Unexpected response type:", type(myresponse))
+
+    return response_string
